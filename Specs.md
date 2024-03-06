c88ef5559c2191771318f9321d5b003051520328
Di <dihuynh2302@gmail.com>
2024-03-05 19:35:55 -0500

    WIP: generate cage

--- Specs.md
+++ Specs.md
@@ -37,5 +37,31 @@ Implement and train an RL agent to solve Killer Sudoku puzzles.
 
 * Generate a complete Sudoku grid
 * Randomly select a subset of cells to remove, resulting in a masked grid
-* Define cages:
-  * 
+
+### Cage Generation Algorithm
+
+1. Select the starting point and size for a cage.
+
+   Top priority for a starting point is a cell that is surrounded on three or four sides, otherwise the cell is chosen at random from the list of unused cells.s
+
+   A cell surrounded on four sides must become a single-cell cage, with a pre-determined value and no operator. Choosing a cell surrounded on three sides allows the cage to occupy and grow out of tight corners, avoiding an excess of small and single-cell cages.
+
+   The chosen size is initially 1 (needed to control the difficulty of the puzzle) and later a random number from 2 to the maximum cage-size. The maximum cage-size also affects difficulty.
+
+2. Use the function `makeOneCage()` to make a cage of the required size.
+
+   The `makeOneCage()` function keeps adding unused neighbouring cells until the required size is reached or it runs out of space to grow the cage further. It updates the lists of used cells and neighbours as it goes. 
+
+    A neighbour that would otherwise become surrounded on all four sides is always added to the cage as it grows, but normally the next cell is chosen randomly.
+
+3. Use the function `setCageTarget()` to choose an operator (+*-/)
+
+    Calculate the cage's value from the cell-values in the puzzle's solution and find all the possible combinations of values that cells in the cage *might* have, as seen by the user.
+
+   The possible combinations are used when solving the generated puzzle, using the DLX algorithm, to check that the puzzle has a unique solution. Many generated puzzles have multiple solutions and have to be discarded.
+
+4. Validate the cage, using function `isCageOK()`.
+
+   A cage can be rejected if it might make the puzzle too hard or too easy. If so, discard the cage, back up and repeat steps 1 to 4.
+
+5. Repeat steps 1 to 4 until all cells have been assigned to cages.
--- sudoku.py
+++ sudoku.py
@@ -4,20 +4,19 @@ import logging
 import copy
 import random
 
-Grid = List[List[int]]
-Cell = Tuple[int, int]
-# Cage = List[Cell]  # A cage is a group of cells that must sum to a certain value
-
 SEED = 42
 RATE = 0.5
+random.seed(SEED)  # for reproducibility
+
+Grid = List[List[int]]
 
 
 class KSudoku:
     def __init__(self, seed=SEED, mask_rate=RATE):
         self.seed = seed
         self.mask_rate = mask_rate
-        self.base = self._generateCompleteGrid()
-        self.grid = self._generateGrid(self.base)
+        self.base: Grid = self._generateCompleteGrid()  # solution
+        self.grid: Grid = self._generateGrid(self.base)
 
     def getGrid(self) -> Grid:
         return self.grid
@@ -25,8 +24,8 @@ class KSudoku:
     def getBase(self) -> Grid:
         return self.base
 
-    def getCell(self, row: int, col: int) -> int:
-        return self.grid[row][col]
+    def getCell(self, r: int, c: int) -> int:
+        return self.grid[r][c]
 
     def _generateGrid(self, baseGrid: Grid) -> Grid:
         return self._mask(baseGrid)
@@ -99,27 +98,31 @@ class KSudoku:
 
 class Cage:
     def __init__(self):
-        self.cells: Optional[List[Cell]] = []
-        self._value: int = 0
+        self.cells: List[Tuple[int, int]] = []  # list of (row, col)
+        self.value: int = 0  # sum of the cells in the cage
 
-    def addCell(self, cell: Cell):
+    def addCell(self, cell: Tuple[int, int]) -> None:
         self.cells.append(cell)
 
-    def removeCell(self, cell: Cell):
+    def removeCell(self, cell: Tuple[int, int]) -> None:
         self.cells.remove(cell)
 
-    def getValue(self) -> int:
-        self._computeValue()
-        return self._value
-
-    def _computeValue(self):
-        self._value = 0
-        for r, c in self.cells:
-            self._value += self.cells[r][c]
+    def getValue(self, baseGrid: Grid) -> int:
+        """
+        Compute the sum of all cells in the cage.
+        :return: the sum of all cells in the cage
+        :rtype: int
+        """
+        for cell in self.cells:
+            row, col = cell
+            self.value += baseGrid[row][col]
+        return self.value
 
 class CageGenerator:
-    def __init__(self, grid: Grid):
-        self.grid = grid
+    def __init__(self, baseGrid: Grid):
+        self.grid = baseGrid
+        self.cages: List[Cage] = []
+        self.unusedCells = [(r, c) for r in range(9) for c in range(9)]
 
     def generateCages(self) -> List[Cage]:
         """
@@ -127,22 +130,158 @@ class CageGenerator:
         :return: the list of cages
         :rtype: List[Cage]
         """
+
+        while self.unusedCells:
+            cell = self._selectCell()
+            size = self._selectSize(cell)
+            cage = self._makeOneCage(cell, size)
+            target = self._getCageTarget(cage)
+            if self._isCageOK(cage):
+                self.cages.append(cage)
+            else:
+                self._backUp(cage)
+
+        return self.cages
+
+    def _selectCell(self) -> Tuple[int, int]:
+        """
+        Select a cell to start a new cage.
+        Top priority for a starting cell is that it is surrounded on 3 or 4 sides,
+        otherwise the cell is chosen at random from the list of unused cells
+        :return: the selected cell
+        :rtype: Tuple[int, int]
+        """
+        if not self.unusedCells:
+            raise ValueError("no unused cells")
+
+        for cell in self.unusedCells:
+            if self._getNumNeighbours(cell) == 3 or self._getNumNeighbours(cell) == 4:
+                return cell
+
+        return random.choice(self.unusedCells)
+
+    def _getNumNeighbours(self, cell: Tuple[int, int]) -> int:
+        """
+        Get the number of neighbours of the given cell.
+        :param cell: the given cell
+        :type cell: Tuple[int, int]
+        :return: the number of neighbours of the given cell
+        :rtype: int
+        """
         pass
+
+    def _selectSize(self, cell: Tuple[int, int]) -> int:
+        """
+        Select the size of a new cage.
+        A cell surrounded on 4 sides must become a single-cell cage.
+        choosing a cell surrounded on three sides allows the cage to occupy and
+        grow out of tight corners, avoiding an excess of small and single-cell cages.
+        The chosen size is a random number from 2 to the maximum space available for the cell.
+        :param cell: the starting cell
+        :type cell: Tuple[int, int]
+        :return: the selected size
+        :rtype: int
+        """
+        numNeighbors = self._getNumNeighbours(cell)
+        if numNeighbors == 4:
+            return 1
+
+        # random size from 2 to the maximum space available for the cell
+        return random.randint(1, numNeighbors + 1)
+
+    def _makeOneCage(self, cell: Tuple[int, int], size: int) -> Cage:
+        """
+        Make a cage of the required size.
+        Keep adding unused neighbouring cells until the required size is reached
+        Update the lists of used cells and neighbours as it goes
+        :param cell: the starting cell
+        :type cell: Tuple[int, int]
+        :param size: the required size
+        :type size: int
+        :return: the cage
+        :rtype: Cage
+        """
+        currentCell = cell
+
+        cage = Cage()
+        cage.addCell(currentCell)
+        unusedNeighbors = self._getUnusedNeighbors(cell)
+
+        while len(cage.cells) < size:
+            currentCell = self._selectNextCell(unusedNeighbors)
+            cage.addCell(currentCell)
+            # update the list of un-used cells and neighbours
+            self._removeUsedCell(currentCell)
+            unusedNeighbors.remove(currentCell)
+            unusedNeighbors += self._getUnusedNeighbors(currentCell)
+            # remove duplicates
+            unusedNeighbors = list(set(unusedNeighbors))
+
+        return cage
+
+    def _selectNextCell(self, neighbors) -> Tuple[int, int]:
+        """
+        Select the next cell to add to the cage.
+        The next cell is selected from the unused neighbours.
+        If a neighbour has 4 neighbours of its own, it is chosen.
+        Otherwise, a random neighbour is chosen.
+        :param neighbors: the list of unused neighbours
+        :type neighbors: List[Tuple[int, int]]
+        :return: the next cell
+        :rtype: Tuple[int, int]
+        """
+        for neighbor in neighbors:
+            if self._getNumNeighbours(neighbor) == 4:
+                return neighbor
+
+        return random.choice(neighbors)
+
+    def _getUnusedNeighbors(self, cell: Tuple[int, int]) -> List[Tuple[int, int]]:
+        """
+        Get the unused neighbours of the given cell.
+        :param cell: the given cell
+        :type cell: Tuple[int, int]
+        :return: the list of unused neighbours
+        :rtype: List[Tuple[int, int]]
+        """
+        pass
+
+    def _getCageTarget(self, cage: Cage) -> int:
+        """
+        Set value of the cage.
+        :param cage: the cage
+        :type cage: Cage
+        """
+        return cage.getValue(self.grid)
+
+    def _isCageOK(self, cage: Cage) -> bool:
+        """
+        Check if the cage is valid.
+        :param cage: the cage
+        :type cage: Cage
+        :return: whether the cage is valid
+        :rtype: bool
+        """
+        pass
+
+    def _removeUsedCell(self, cell: Tuple[int, int]) -> None:
+        """
+        Remove the used cell from the unused cells.
+        :param cell: the cell
+        :type cell: Tuple[int, int]
+        """
+        self.unusedCells.remove(cell)
 
 
 if __name__ == "__main__":
-    # ks = KSudoku()
-    # g = ks.getGrid()
-    # for r in g:
-    #     print(r)
-    #
-    # print()
-    #
-    # b = ks.getBase()
-    # for r in b:
-    #     print(r)
+    ks = KSudoku()
+    g = ks.getGrid()
+    for r in g:
+        print(r)
+    print()
 
-    cg = Cage()
-    cg.addCell((1, 2))
-    cg.addCell((3, 4))
-    print(cg.cells)
+    b = ks.getBase()
+    for r in b:
+        print(r)
+
+    cg = CageGenerator(b)
